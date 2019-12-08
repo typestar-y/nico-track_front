138365f241d01db555ba5d17849e1fd3a76b1dfe
kanasy <29543015+kanasy@users.noreply.github.com>
2019-12-08 19:10:53 +0900

    update: ボタンに応じて、typeを設定できるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,21 @@
   <v-row justify="space-around">
     <v-col>
       <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling" large>
+        <v-btn @click="registerFeeling(1)" large>
           1
         </v-btn>
       </v-row>
     </v-col>
     <v-col>
       <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling" large>
+        <v-btn @click="registerFeeling(2)" large>
           2
         </v-btn>
       </v-row>
     </v-col>
     <v-col>
       <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling" large>
+        <v-btn @click="registerFeeling(3)" large>
           3
         </v-btn>
       </v-row>
@@ -28,10 +28,10 @@
 export default {
   name: 'Index',
   methods: {
-    async registerFeeling() {
+    async registerFeeling(type) {
       await this.$axios.$post('http://localhost:8083/feelings', {
         date: '2019-09-10',
-        type: 1
+        type
       })
     }
   }
