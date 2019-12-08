c3c29affe95743ba52e03f64a8e24d571235b233
kanasy <29543015+kanasy@users.noreply.github.com>
2019-12-08 18:57:56 +0900

    update: ボタン追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-    test
-  </div>
+  <v-row justify="space-around">
+    <v-col>
+      <v-row align="center" justify="center">
+        <v-btn @click="registerFeeling" large>
+          1
+        </v-btn>
+      </v-row>
+    </v-col>
+    <v-col>
+      <v-row align="center" justify="center">
+        <v-btn @click="registerFeeling" large>
+          2
+        </v-btn>
+      </v-row>
+    </v-col>
+    <v-col>
+      <v-row align="center" justify="center">
+        <v-btn @click="registerFeeling" large>
+          3
+        </v-btn>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+  methods: {
+    async registerFeeling() {
+      await this.$axios.$post('http://localhost:8083/feelings', {
+        date: '2019-09-10',
+        type: 1
+      })
+    }
+  }
 }
 </script>
 
