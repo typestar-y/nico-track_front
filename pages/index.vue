d12a93d7676594c050692321194a654380f84f7a
kanasy <29543015+kanasy@users.noreply.github.com>
2019-12-14 18:35:57 +0900

    update: 一覧を表示する機能を追加した

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <v-row justify="space-around">
-    <v-col v-for="type in types" :key="type.num">
-      <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling(type.num)" large>
-          <v-icon>{{ type.iconName }}</v-icon>
-        </v-btn>
-      </v-row>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row justify="space-around">
+      <v-col v-for="type in types" :key="type.num">
+        <v-row align="center" justify="center">
+          <v-btn @click="registerFeeling(type.num)" large>
+            <v-icon>{{ type.iconName }}</v-icon>
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn @click="searchFeeling()"></v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <ul>
+          <li v-for="(feeling, index) in feelings" :key="index">
+            {{ feeling.date }} : {{ feeling.type }}
+          </li>
+        </ul>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -19,7 +35,8 @@ export default {
         { iconName: 'mdi-emoticon-excited-outline', num: 1 },
         { iconName: 'mdi-emoticon-outline', num: 2 },
         { iconName: 'mdi-emoticon-frown-outline', num: 3 }
-      ]
+      ],
+      feelings: ''
     }
   },
   methods: {
@@ -28,6 +45,10 @@ export default {
         date: '2019-09-10',
         type
       })
+    },
+    async searchFeeling() {
+      const result = await this.$axios.$get('http://localhost:8083/feelings')
+      this.feelings = result.feelings
     }
   }
 }
