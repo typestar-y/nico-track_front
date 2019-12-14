f3e0cef974099864d5097f68f1b0225028efdf4c
kanasy <29543015+kanasy@users.noreply.github.com>
2019-12-14 18:21:06 +0900

    update: ボタンにアイコンを表示するようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,9 @@
 <template>
   <v-row justify="space-around">
-    <v-col>
+    <v-col v-for="type in types" :key="type.num">
       <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling(1)" large>
-          1
-        </v-btn>
-      </v-row>
-    </v-col>
-    <v-col>
-      <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling(2)" large>
-          2
-        </v-btn>
-      </v-row>
-    </v-col>
-    <v-col>
-      <v-row align="center" justify="center">
-        <v-btn @click="registerFeeling(3)" large>
-          3
+        <v-btn @click="registerFeeling(type.num)" large>
+          <v-icon>{{ type.iconName }}</v-icon>
         </v-btn>
       </v-row>
     </v-col>
@@ -27,6 +13,15 @@
 <script>
 export default {
   name: 'Index',
+  data() {
+    return {
+      types: [
+        { iconName: 'mdi-emoticon-excited-outline', num: 1 },
+        { iconName: 'mdi-emoticon-outline', num: 2 },
+        { iconName: 'mdi-emoticon-frown-outline', num: 3 }
+      ]
+    }
+  },
   methods: {
     async registerFeeling(type) {
       await this.$axios.$post('http://localhost:8083/feelings', {
